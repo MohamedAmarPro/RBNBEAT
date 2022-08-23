401e8c9472cc6cf13267d4e910d386573806da69
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2022-08-23 11:34:29 +0200

    create action new/create et view/new

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,18 @@
 class InstrumentsController < ApplicationController
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(params_instruments)
+    if @instrument.save
+      redirect_to instruments_path(@instrument)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def params_instruments
+    params.require(:instrument).permit(:name, :category, :brand, :age, :brand, photos: [])
+  end
 end
