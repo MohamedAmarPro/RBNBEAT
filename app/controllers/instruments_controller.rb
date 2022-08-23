e510fd06082e438098fa1aae2ef7919b355054f2
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2022-08-23 12:39:28 +0200

    create actions edit/destroy and edit/view

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,5 +1,5 @@
 class InstrumentsController < ApplicationController
-  before_action :set_instrument, only: :show
+  before_action :set_instrument, only: [:show, :destroy, :edit]
 
   def index
     @instruments = Instrument.all
@@ -20,13 +20,20 @@ class InstrumentsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def destroy
+    @instrument.destroy
+  end
+
+  def edit
+  end
+
   private
 
   def set_instrument
     @instrument = Instrument.find(params[:id])
   end
-  
+
   def params_instruments
     params.require(:instrument).permit(:name, :category, :brand, :age, :brand, photos: [])
   end
