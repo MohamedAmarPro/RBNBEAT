f032d4578b2c34f62225798a4c552268a7395fbf
Alan Laurier <alanlaurier@sfr.fr>
2022-08-23 13:37:43 +0200

    photos and seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,26 +16,26 @@ momo = User.create(email: 'momo@lewagon.com', password: 'password')
 
 file = URI.open('http://loremflickr.com/400/400/guitar')
 instrument = Instrument.new(name: 'Guitar', category: 'Instrument a corde', brand: 'Yamaha', age: '5 months', price: '5$', user: alan)
-instrument.photo.attach(io: file, filename: 'guitar.png', content_type: 'image/png')
+instrument.photos.attach(io: file, filename: 'guitar.png', content_type: 'image/png')
 
 file2 = URI.open('http://loremflickr.com/400/400/piano')
 instrument2 = Instrument.new(name: 'Piano', category: 'Instrument a corde frappées', brand: 'Bechstein', age: '12 months', price: '5$', user: alan)
-instrument2.photo.attach(io: file2, filename: 'piano.png', content_type: 'image/png')
+instrument2.photos.attach(io: file2, filename: 'piano.png', content_type: 'image/png')
 
 file3 = URI.open('http://loremflickr.com/400/400/violon')
 instrument3 = Instrument.new(name: 'Violon', category: 'Instrument a corde', brand: 'Stradivarius', age: '5 months', price: '5$', user: alan)
-instrument3.photo.attach(io: file3, filename: 'piano.png', content_type: 'image/png')
+instrument3.photos.attach(io: file3, filename: 'piano.png', content_type: 'image/png')
 
 file4 = URI.open('http://loremflickr.com/400/400/flute')
 instrument4 = Instrument.new(name: 'Flute', category: 'Instrument a vent en cuivre', brand: 'Cora', age: '1 months', price: '5$', user: alan)
-instrument4.photo.attach(io: file4, filename: 'flute.png', content_type: 'image/png')
+instrument4.photos.attach(io: file4, filename: 'flute.png', content_type: 'image/png')
 
 file5= URI.open('http://loremflickr.com/400/400/drum')
 instrument5 = Instrument.new(name: 'Drum', category: 'Instrument percussion', brand: 'Yamaha', age: '7 months', price: '5$', user: alan)
 instrument5.photos.attach(io: file5, filename: 'drum.png', content_type: 'image/png')
 
-instrument.save
-instrument2.save
-instrument3.save
-instrument4.save
-instrument5.save
+instrument.save!
+instrument2.save!
+instrument3.save!
+instrument4.save!
+instrument5.save!
