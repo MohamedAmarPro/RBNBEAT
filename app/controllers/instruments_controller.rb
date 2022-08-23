d51b7018c609e302b06ecef8ea6b9c03e76a3916
Titouan Andritsos <titouan.andritsos@hotmail.fr>
2022-08-23 11:34:43 +0200

    index show and views for instruments created

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,16 @@
 class InstrumentsController < ApplicationController
+  before_action :set_instrument, only: :show
+
+  def index
+    @instruments = Instrument.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
 end
